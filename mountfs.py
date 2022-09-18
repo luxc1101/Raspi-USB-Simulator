5177b3fc29687473da9597505a0186b1f3d26abe
Xiaochuan Lu <xiaochuan_lu@yahoo.com>
2022-09-18 23:39:18 +0200

    recursive algo

--- mountfs.py
+++ mountfs.py
@@ -15,101 +15,119 @@ import fsc
 #       Paramters        #
 ##########################
 # img file dict
-FileImgDic={}
-FileImgDic[0]="mib_compliance.img" # fat32
-FileImgDic[1]="ext2.img"
-FileImgDic[2]="ext3.img"
-FileImgDic[3]="ext4.img"
-FileImgDic[4]="fat16.img"
-FileImgDic[5]="fat32.img"
-FileImgDic[6]="ntfs.img"
-FileImgDic[7]="exfat.img"
-FileImgDic[8]="hfsplus.img"
-FileImgDic[9]="part.img"
-FileImgDic[10]="sw.img" # ntfs
+FileImgDic = {}
+FileImgDic[0] = "mib_compliance.img"  # fat32
+FileImgDic[1] = "ext2.img"
+FileImgDic[2] = "ext3.img"
+FileImgDic[3] = "ext4.img"
+FileImgDic[4] = "fat16.img"
+FileImgDic[5] = "fat32.img"
+FileImgDic[6] = "ntfs.img"
+FileImgDic[7] = "exfat.img"
+FileImgDic[8] = "hfsplus.img"
+FileImgDic[9] = "part.img"
+FileImgDic[10] = "sw.img"  # ntfs
 # mount point dict
-MPDic={}
-MPDic[0]="/mnt/usb_MIB_Compliance"
-MPDic[1]="/mnt/usb_ext2"
-MPDic[2]="/mnt/usb_ext3"
-MPDic[3]="/mnt/usb_ext4"
-MPDic[4]="/mnt/usb_fat16"
-MPDic[5]="/mnt/usb_fat32"
-MPDic[6]="/mnt/usb_ntfs"
-MPDic[7]="/mnt/usb_exfat"
-MPDic[8]="/mnt/usb_hfsplus"
-MPDic[9]="/mnt/usb_part_fat32"
-MPDic[10]="/mnt/usb_sw"
+MPDic = {}
+MPDic[0] = "/mnt/usb_MIB_Compliance"
+MPDic[1] = "/mnt/usb_ext2"
+MPDic[2] = "/mnt/usb_ext3"
+MPDic[3] = "/mnt/usb_ext4"
+MPDic[4] = "/mnt/usb_fat16"
+MPDic[5] = "/mnt/usb_fat32"
+MPDic[6] = "/mnt/usb_ntfs"
+MPDic[7] = "/mnt/usb_exfat"
+MPDic[8] = "/mnt/usb_hfsplus"
+MPDic[9] = "/mnt/usb_part_fat32"
+MPDic[10] = "/mnt/usb_sw"
 
 diclen = len(FileImgDic)
-#color
+# color
 # sys.stdout.write("{}hallo".format(Cyan))
-Cyan='\033[1;96m'
-Yellow='\033[1;93m'
-Green='\033[1;92m'
-Red='\033[1;91m'
-C_off='\033[0m'
+Cyan = '\033[1;96m'
+Yellow = '\033[1;93m'
+Green = '\033[1;92m'
+Red = '\033[1;91m'
+C_off = '\033[0m'
 
 ##########################
 #       Functions        #
 ##########################
-def getfsname(img:str):
+
+
+def getfsname(img: str):
     return img.split(".")[0]
 
-### remount filesystem
+# remount filesystem
+
+
 def remount(file):
     os.system('sudo /sbin/modprobe g_multi -r')
     time.sleep(2)
-    os.system('sudo /sbin/modprobe g_multi file=./{} stall=0 removable=1'.format(file))
+    os.system(
+        'sudo /sbin/modprobe g_multi file=./{} stall=0 removable=1'.format(file))
 
-#### check if a package installed or not and try to install it
-def installcheck(PKG:str):
-    PKG_OK = "dpkg-query -W --showformat='${Status}\n' " + "{}|grep 'install ok installed'".format(PKG)
-    sys.stdout.write("Checking for {}: {}{}{}\n".format(PKG, Red, os.popen(PKG_OK).read().split("\n")[0],C_off))
+# check if a package installed or not and try to install it
+
+
+def installcheck(PKG: str):
+    PKG_OK = "dpkg-query -W --showformat='${Status}\n' " + \
+        "{}|grep 'install ok installed'".format(PKG)
+    sys.stdout.write("Checking for {}: {}{}{}\n".format(
+        PKG, Red, os.popen(PKG_OK).read().split("\n")[0], C_off))
     status = True
     if os.popen(PKG_OK).read() == "":
         status = False
-        sys.stdout.write("going to install {}{}{} first\n".format(Red, PKG, C_off))
+        sys.stdout.write(
+            "going to install {}{}{} first\n".format(Red, PKG, C_off))
         Install = 'sudo apt-get --yes install {}'.format(PKG)
         os.system(Install)
     return status
 
-### configuration of samba conf file       
-def sambaconf(PKG:str, img:str, MP:str):
+# configuration of samba conf file
+
+
+def sambaconf(PKG: str, img: str, MP: str):
     conf = "/etc/samba/smb.conf"
     status = installcheck(PKG)
     if not status:
         # after installation first init the samba conf file
-        os.system("sudo sed -i -e '$a [raspiusb_{}]' {}".format(getfsname(img),conf))
+        os.system(
+            "sudo sed -i -e '$a [raspiusb_{}]' {}".format(getfsname(img), conf))
         os.system("sudo sed -i -e '$a browseable = yes' {}".format(conf))
         os.system("sudo sed -i -e '$a guest ok = yes' {}".format(conf))
         os.system("sudo sed -i -e '$a creat mask = 0777' {}".format(conf))
         os.system("sudo sed -i -e '$a read only = no' {}".format(conf))
         os.system("sudo sed -i -e '$a writeable = yes' {}".format(conf))
-        os.system("sudo sed -i -e '$a path = {}' {}".format(MP,conf))
+        os.system("sudo sed -i -e '$a path = {}' {}".format(MP, conf))
     else:
-        # rewrite folder path and rename remote folder 
-        lineNr = os.popen("grep -n 'raspiusb' {} | cut -d: -f1".format(conf)).read().split("\n")[0]
+        # rewrite folder path and rename remote folder
+        lineNr = os.popen(
+            "grep -n 'raspiusb' {} | cut -d: -f1".format(conf)).read().split("\n")[0]
         # print ("the lineNr is: {}".format(lineNr))
-        os.system("sudo sed -i '{}s/.*/[raspiusb_{}]/' {}".format(lineNr,getfsname(img), conf))
+        os.system(
+            "sudo sed -i '{}s/.*/[raspiusb_{}]/' {}".format(lineNr, getfsname(img), conf))
         os.system("sudo sed -i '/mnt/d' {}".format(conf))
-        os.system("sudo sed -i -e '$a path = {}' {}".format(MP,conf))
-    
+        os.system("sudo sed -i -e '$a path = {}' {}".format(MP, conf))
+
     os.system("sudo systemctl restart smbd.service")
     sys.stdout.write("{}status of samba service: \n".format(Cyan))
-    sys.stdout.write("{}{}".format(Yellow,os.popen("sudo systemctl status smbd.service | grep -E 'Loaded|Active|Status'").read()))
+    sys.stdout.write("{}{}".format(Yellow, os.popen(
+        "sudo systemctl status smbd.service | grep -E 'Loaded|Active|Status'").read()))
     sys.stdout.write("{}Network access: ".format(Cyan))
-    sys.stdout.write("{}Hostname and IP: {} {}\n".format(Yellow, os.popen('hostname').read().split("\n")[0], os.popen('hostname -I').read().split("\n")[0]))
+    sys.stdout.write("{}Hostname and IP: {} {}\n".format(Yellow, os.popen(
+        'hostname').read().split("\n")[0], os.popen('hostname -I').read().split("\n")[0]))
     os.system('sudo chmod 777 {}'.format(MP))
     sys.stdout.write("{}file in {}: \n".format(Cyan, MP))
     for f in os.listdir(MP):
-        path = os.path.join(MP,f)
+        path = os.path.join(MP, f)
         if os.path.isdir(path):
             sys.stdout.write(Yellow + "-" + f + " d" + "\n")
         elif os.path.isfile(path):
             sys.stdout.write(Yellow + "-" + f + " f" + "\n")
         else:
             sys.stdout.write(Yellow + "-" + f + " u" + "\n")
+
 
 def menu():
     sys.stdout.write(Cyan + "Please select one filesystem to mount" + "\n")
@@ -126,21 +144,68 @@ def menu():
     sys.stdout.write("10: Software update" + "\n")
     sys.stdout.write(Green + "r: remount" + "\n")
     sys.stdout.write("q: quit and eject the usb" + "\n")
-    sys.stdout.write("c: cancel or terminate the currently running program" + C_off + "\n")
+    sys.stdout.write(
+        "c: cancel or terminate the currently running program" + C_off + "\n")
 
 
-fsc.Cfilesystem("fat32.img", "/mnt/usb_fat32")
+# fsc.Cfilesystem("fat32.img", "/mnt/usb_fat32")
+##########################
+#     Recursive Algo     #
+##########################
+def USBSIM(FileImgDic, MPDic):
+    Imgdic = FileImgDic
+    MPdic = MPDic
+
+    def checkinput(Input):
+        if (Input.lower() not in ["r", "q", "c"]) and (Input not in [str(i) for i in range(diclen)]):
+            print(Red + "Warning: " + "invalid input, retry to enter" + C_off)
+            return False
+        return True
+
+    def lsblk():
+        print(Cyan + "=" * 60 + Yellow)
+        os.system("lsblk -f | grep 'loop'")
+        print(Cyan + "=" * 60)
+
+    try:
+        menu()
+        Input = input(Cyan + "Enter a value: " + C_off)
+        # base case: invalid input
+        if not checkinput(Input):
+            return USBSIM(FileImgDic, MPDic)
+        # base case: remount
+        elif Input.lower() == "r":
+            print(Cyan + "list the already remounted filesytems")
+            lsblk()
+            Input = input(
+                Cyan + "which filesystem will be remounted: " + C_off)
+            if checkinput(Input):
+                print(Cyan + "remount " + Red +
+                      Imgdic[int(Input)] + Cyan + " filesystem")
+                remount(Imgdic[int(Input)])
+        # base case: cancel currently programm
+        elif Input.lower() == "c":
+            print(Cyan + "terminate the programm")
+            lsblk()
+            return
+        # base case: quit and eject
+        elif Input.lower() == "q":
+            print(Cyan + "terminate the programm and eject")
+            lsblk()
+            os.system('sudo /sbin/modprobe g_multi -r')  # unmount first
+            return
+        # base case: USB simulator
+        else:
+            fsname = getfsname(Imgdic[int(Input)])
+            lsimg = Imgdic[int(Input)].lower()
+            MPpath = MPdic[int(Input)]
+            print(Cyan + "prepare to mount " + Red + format + C_off)
+
+            return USBSIM(FileImgDic, MPDic)
+
+    except KeyboardInterrupt:
+        print(KeyboardInterrupt)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    USBSIM(FileImgDic, MPDic)
